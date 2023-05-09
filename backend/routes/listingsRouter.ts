4475b2e63eba6383cdaa87008dda34901951eeda
Ahmad Ali <ahmad12893@hotmail.com>
2023-05-09 14:12:44 +0100

    feat: routes for listings

---
+++ backend/routes/listingsRouter.ts
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+import {
+  createListings,
+  fetchListings,
+  getListingsById,
+  updateListings,
+  softDeleteListings,
+  hardDeleteListings,
+} from '../controllers/listingsController';
+
+const router = Router();
+
+router.post('/', createListings);
+router.get('/', fetchListings);
+router.get('/:id', getListingsById);
+router.put('/:id', updateListings);
+router.delete('/:id/soft', softDeleteListings);
+router.delete('/:id', hardDeleteListings);
+
+export default router;
