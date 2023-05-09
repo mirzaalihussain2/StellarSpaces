1766ae6963d889be4d8c3cbe721c68b7f6554050
Ahmad Ali <ahmad12893@hotmail.com>
2023-05-09 08:39:23 +0100

    feat: added listings model

---
+++ backend/models/listingsModel.ts
@@ -0,0 +1,82 @@
+import { Listing } from '../interfaces/Listing';
+
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+// Create a new listing
+async function createListing(data: Listing) {
+  return await prisma.listing.create({
+    data,
+  });
+}
+
+// Get all listings
+async function getListings() {
+  return await prisma.listing.findMany({
+    where: {
+      deletedAt: null,
+    },
+  });
+}
+
+// Get a listing by ID
+async function getListingById(id: Listing['id']) {
+  return await prisma.listing.findUnique({
+    where: {
+      id: id,
+    },
+  });
+}
+
+// Update a listing by ID
+async function updateListing(id: Listing['id'], data: Listing) {
+  return await prisma.listing.update({
+    where: {
+      id: id,
+    },
+    data,
+  });
+}
+
+// Soft delete a listing by ID
+async function softDeleteListing(id: Listing['id']) {
+  const listing = await prisma.listing.update({
+    where: { id: id },
+    data: { deletedAt: new Date() },
+    include: {
+      Image: {
+        update: { data: { deletedAt: new Date() }, where: { deletedAt: null } },
+      },
+      favourites: {
+        update: { data: { deletedAt: new Date() }, where: { deletedAt: null } },
+      },
+      chats: {
+        update: { data: { deletedAt: new Date() }, where: { deletedAt: null } },
+      },
+    },
+  });
+  return listing;
+}
+
+// Hard delete a listing by ID
+async function hardDeleteListing(id: Listing['id']) {
+  const listing = await prisma.listing.delete({
+    where: { id: id },
+    include: {
+      Image: true,
+      favourites: true,
+      chats: true,
+    },
+  });
+  return listing;
+}
+
+// Export the CRUD operations
+module.exports = {
+  createListing,
+  getListings,
+  getListingById,
+  updateListing,
+  softDeleteListing,
+  hardDeleteListing,
+};
