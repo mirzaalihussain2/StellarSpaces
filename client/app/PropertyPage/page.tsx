bc70ac2707841c9965fbe14fa9e104cb4920f1be
Majeed <abdulmajeed.hersi@gmail.com>
2023-05-10 14:36:09 +0100

    small changes before code review

---
+++ client/app/PropertyPage/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+import './page.css';
+import NavBar from '../components/NavBar'
+
+
+export default function PropertyPage() {
+    return (
+        <div>
+            <NavBar />
+            
+        </div>
+    )
+}
