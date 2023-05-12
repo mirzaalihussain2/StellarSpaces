0df29184c98b5f0534bae6171a6b593468785c93
Ahmad Ali <ahmad12893@hotmail.com>
2023-05-12 16:03:32 +0100

    feat: added image routes for create and delete

---
+++ backend/routes/imagesRouter.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import { create, remove } from '../controllers/imagesController';
+import { authenticateJwt } from '../middleware/auth';
+
+const router = Router();
+
+// Route to add an image to a listing
+router.post('/listings/:listingId/images', authenticateJwt, create);
+
+// Route to delete an image from a listing
+router.delete('/listings/:listingId/images/:imageId', authenticateJwt, remove);
+
+export default router;
