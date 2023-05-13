3911c17c8cf109c31f898a257459058aa734a8e5
mirzaalihussain2 <mirzaalihussain2@gmail.com>
2023-05-13 14:06:40 +0100

    Empty Databse utility added. Works as a generic function, deleting records for all tables, with success msg and error-handling.

--- backend/utilities/emptyDatabase.js
+++ backend/utilities/emptyDatabase.js
@@ -2,46 +2,20 @@
 const { PrismaClient } = require('@prisma/client');
 let prisma = new PrismaClient();
 
-// // Empty favourites table
-// async function deleteAllFavourites () {
-//   await prisma.favourites.deleteMany({});
-//   const successMsg = `All favourites successfully deleted`;
-//   console.log(successMsg);
-//   res.status(200).json({message: successMsg});
-// };
-
-// // Empty images table
-// async function deleteAllImages () {
-//   await prisma.images.deleteMany({});
-//   const successMsg = `All favourites successfully deleted`;
-//   console.log(successMsg);
-//   res.status(200).json({message: successMsg});
-// };
-
-// // Empty users table
-// async function deleteAllUsers () {
-//   await prisma.user.deleteMany({});
-//   const successMsg = `All users successfully deleted`;
-//   console.log(successMsg);
-//   // res.status(200).json({message: successMsg});
-// };
-
+// Function to delete all records in table, with table and tableName (string) arguments
 async function deleteRecords (table, tableName) {
   try {
     await table.deleteMany({});
-    console.log(`All ${tableName} successfully deleted`);
+    console.log(`All records in ${tableName} table deleted.`);
   } catch (error) {
     console.log(`Error in deleting ${tableName} table:\n${error}`);
   }
 };
 
-// Deleting records for
+// Deleting records for all tables, starting with most granular
 deleteRecords(prisma.favourites, 'Favourites');
 deleteRecords(prisma.image, 'Images');
 deleteRecords(prisma.message, 'Messages');
 deleteRecords(prisma.chat, 'Chats');
 deleteRecords(prisma.listing, 'Listings');
 deleteRecords(prisma.user, 'Users');
-
-
-
