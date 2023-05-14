93a44ad7a61a7a1d26e1a0894be956bdf37a8904
Ahmad Ali <ahmad12893@hotmail.com>
2023-05-14 16:06:41 +0100

    feat: added user service api

---
+++ client/app/ApiServices/backend/localUserService.ts
@@ -0,0 +1,70 @@
+import axios from 'axios';
+
+// create local user function
+async function createUsers(user: any) {
+  const response = await fetch('/api/users', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(user),
+  });
+
+  if (response.ok) {
+    return response.json();
+  } else {
+    throw new Error('Error: ' + response.statusText);
+  }
+}
+
+// login local user function
+async function loginUser(user: any) {
+  const response = await fetch('/api/users/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(user),
+  });
+
+  if (response.ok) {
+    return response.json();
+  } else {
+    throw new Error('Error: ' + response.statusText);
+  }
+}
+// update user information function
+async function updateUsers(id: any, user: any, token: any) {
+  const response = await fetch(`/api/users/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(user),
+  });
+
+  if (response.ok) {
+    return response.json();
+  } else {
+    throw new Error('Error: ' + response.statusText);
+  }
+}
+
+//delete user function
+async function softDeleteUsers(id: any, token: any) {
+  const response = await fetch(`/api/users/${id}/soft`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  if (response.ok) {
+    return response.json();
+  } else {
+    throw new Error('Error: ' + response.statusText);
+  }
+}
+export { createUsers, loginUser, updateUsers, softDeleteUsers };
