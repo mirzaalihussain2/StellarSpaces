0eb897d20f7e58ad946c99c28fd760f3f6626857
Majeed <abdulmajeed.hersi@gmail.com>
2023-05-15 10:41:14 +0100

    api users

--- client/app/ApiServices/backend/localUserService.ts
+++ client/app/ApiServices/backend/localUserService.ts
@@ -28,7 +28,7 @@ async function loginUser(user: any) {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      Authorization: `Bearer ${token}`,
+      // Authorization: `Bearer ${token}`,
     },
     body: JSON.stringify(user),
   });
